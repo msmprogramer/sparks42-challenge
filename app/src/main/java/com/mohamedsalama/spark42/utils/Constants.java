7c9e9f20f409e02eed4657c4a38ee049e67fa7bb
msalama <msalama@apptitude-eg.com>
2016-05-20 16:11:45 +0200

    init the demo

---
+++ app/src/main/java/com/mohamedsalama/spark42/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.mohamedsalama.spark42.utils;
+
+public final class Constants {
+
+    private Constants() {
+        // no instance
+    }
+
+    //Api Url
+    public static final String BACKEND_API_URL = "";
+}
